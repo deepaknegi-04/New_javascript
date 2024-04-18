a1bcd10b72c814bdf57a92497ff3f925d3f2e65b
Deepak <negideepak@gmail.come>
2024-04-18 14:00:01 +0530

    first commit from vs code

---
+++ js_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treate all code as newer version
+
+//alert("hello"); //we are using node js not browser
+
+let name = "deepak";
+let age = 20
+let is_logged = true;
+
+//number => 2 to power 53
+//bigint
+// string => ""
+//null => standalone value
+//undefined =>
+//symbol
+//objects
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);//object
---
+++ js_basic/03_conversions.js
@@ -0,0 +1,51 @@
+// let score = undefined;
+
+// console.log(typeof score);
+// let innumber = Number(score);
+// console.log(typeof innumber);
+// console.log(innumber);
+
+//"34" => 33
+//"34ac" => NaN
+//true => 1; false => 0
+
+// let num = 1
+// let isnum = Boolean(num)
+// console.log(isnum);
+
+//conversion in boolean
+// 1=> ture; 0=> false
+//"" => false
+//"hitest "=> true
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = "raj"
+
+// let str3 = str1 +" " +str2
+// console.log(str3);
+
+// console.log("1"+11);
+// console.log(1 + 2 +"3");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2; not a good practice
+
+// let game = 100
+// ++game;
+// console.log(game);
+
+
