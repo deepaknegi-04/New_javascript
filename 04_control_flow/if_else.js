c1a7335d24bfa095f74c4a90f78e755f94ebf75d
Deepak <negideepak@gmail.come>
2024-04-26 18:43:27 +0530

    if-else in js

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,41 @@
+// if
+
+// const isUserloggedIn = true
+// const temperature = 30
+
+// if(temperature === 30){
+// console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50");
+// }
+
+// <, >, <=, <=, ==, ===, !=, !==  are used for comparison. 
+
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+//Shorthand
+
+// const balance = 500
+// //Implicit Scope
+// if (balance > 400) console.log("test"), console.log("test two"); //not a good practice
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2 == 3) {
+//     console.log("Allow to but course");
+// }
+
+// if (loggedInFromEmail || loggedInFromGoogle) {
+//     console.log("User logged in");
+
+// }
+
