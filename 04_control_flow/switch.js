409dd33b21e0c94f68b255967014b20a8e8e2391
Deepak <negideepak@gmail.come>
2024-04-26 18:44:17 +0530

    switch in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    default:
+        console.log("Invalid Month")
+        break;
+}
