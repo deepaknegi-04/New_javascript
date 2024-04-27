a324862310898ca60b2621fcd9e08755ba3341f9
Deepak <negideepak@gmail.come>
2024-04-27 19:31:16 +0530

    Map() in js

---
+++ 05_iterations/four.js
@@ -0,0 +1,34 @@
+const myObject = {
+    js:"javaScript",
+    cpp:"C++",
+    rb:"ruby",
+    swift:"swift by Apple"
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut for ${myObject[key]}`);
+// }
+
+// const programming = ["js","c","py"]
+
+// for (const key in programming) {
+//   console.log(programming[key]);
+// }
+
+
+// const map = new Map() //not iterable easily with for in
+// map.set('IN',"India")
+// map.set('USA',"United State of America")
+// map.set('IN',"India")// Holds unique value
+
+// for (const key in map) {
+//   console.log(key);
+// }
+
+
+//generally
+// for of loop - array
+//for in loop - object
+
+
+
