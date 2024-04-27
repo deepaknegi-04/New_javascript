0c8d151f7a31a3e4b56ec694a39e70081400fe50
Deepak <negideepak@gmail.come>
2024-04-27 19:30:16 +0530

    for of loop in js

---
+++ 05_iterations/three.js
@@ -0,0 +1,45 @@
+// for of
+
+// ["", "", ""]
+// [{},{},{}]
+
+// const array = [ 1,2,3,4]
+
+// for (const num of array) {// object != datatype(object)
+   
+//     console.log(num);
+    
+// }
+
+// const greeting = "Hello world!"
+
+// for (const greet of greeting) { 
+//     // if(greet == " "){
+//     //     console.log("Space detected");
+//     //     continue
+//     // }
+//     console.log(`Each char is ${greet}`);
+// }
+
+//Maps
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United State of America")
+// map.set('IN',"India")// Holds unique value
+// console.log(map);
+
+// // for of loop in objects
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+// const myObj = {
+//     game:'NFS',
+//     game2:'Spiderman'
+// }
+// for (const [key, value] of myObj) {
+//     console.log(key,":",value);
+// }  
+//NOT WORKING
