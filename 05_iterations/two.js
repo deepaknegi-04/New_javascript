746cbb59b4ecc2c56efeed8ecf3fd4c0efd175e5
Deepak <negideepak@gmail.come>
2024-04-27 06:38:33 +0530

    while and do-while loops in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0
+
+// while(index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index +=2
+// }
+
+let myArry = ["flash", "batman"]
+
+// let arr = 0
+// while (arr < myArry.length) {
+//     console.log(`value is ${myArry[arr]}`);
+//     arr++;
+// }
+
+let score = 11
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score<=10);
